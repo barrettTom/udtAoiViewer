1dc286eaedaf00f1a8e9a933178e2b0b97c51afd
Tom Barrett <tombarrett@siu.edu>
2017-07-03 10:38:19 -0500

    -cleaning

--- lib/TreeModel.py
+++ lib/TreeModel.py
@@ -8,19 +8,12 @@ class TreeModel(QAbstractItemModel):
     def __init__(self, path, parent=None):
         super(TreeModel, self).__init__(parent)
 
-        self.rootItem = TreeItem(["Hardware", "Hardware Comment", "Connected Parameter", "Parameter Comment"])
 
         self.path = path
 
         self.isHighlighted = False
 
         self.setupModel(path)
-
-    def headerData(self, section, orientation, role):
-        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return self.rootItem.data(section)
-
-        return None
 
     def rowCount(self, parent):
         if parent.column() > 0:
@@ -42,9 +35,6 @@ class TreeModel(QAbstractItemModel):
     def flags(self, index):
         if not index.isValid():
             return Qt.NoItemFlags
-
-        if index.column() == 1 or index.column() == 3:
-            return Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable
         else:
             return Qt.ItemIsEnabled | Qt.ItemIsSelectable
 
@@ -104,192 +94,36 @@ class TreeModel(QAbstractItemModel):
 
         return item.data(index.column())
 
-    def findHW(self, dataPath):
-        m = dataPath[0]
-        e = dataPath[-1][1:]
-
-        for module in self.root.iter("Module"):
-            if module.attrib['Name'] == m:
-                for comment in module.iter("Comment"):
-                    if comment.attrib['Operand'] == e:
-                        return comment
-
-        m = ":".join(dataPath[0:2])
-
-        for module in self.root.iter("Module"):
-            address = module.findall("./Ports/")[0].attrib["Address"]
-            hardware = module.attrib['ParentModule']
-            hardware = hardware + ":" + address
-            if hardware == m:
-                for comment in module.iter("Comment"):
-                    if comment.attrib['Operand'] == e:
-                        return comment
-
-    def findPA(self, dataPath):
-        p = dataPath[0][1:]
-        t = dataPath[1]
-
-        for program in self.root.iter("Program"):
-            if program.attrib['Name'] == p:
-                for tag in program.iter("Tag"):
-                    if tag.attrib['Name'] == t:
-                        potential = tag.findall("Description")
-                        if len(potential):
-                            return potential[0]
-                        else:
-                            element =ET.Element("Description")
-                            tag.insert(0, element)
-                            return element
-
-    def fixComments(self):
-        for rung in self.root.iter("Rung"):
-            gottaFix = rung.findall("Comment")
-            if gottaFix:
-                gottaFix = gottaFix[0]
-                text = gottaFix.text
-                text = text.strip()
-                text = text.replace("\n", " \n")
-                gottaFix.text = ET.CDATA(text)
+    def highlight(self):
+        self.isHighlighted = not self.isHighlighted
 
     def setupModel(self, path):
         self.parser = ET.XMLParser(strip_cdata=False, resolve_entities=False)
         self.tree = ET.parse(path, parser=self.parser)
         self.root = self.tree.getroot()
 
+        self.rootItem = TreeItem(["Hardware", "Hardware Comment", "Connected Parameter"])
+
         self.base = TreeItem([path.split('/')[-1]], self.rootItem)
         self.rootItem.appendChild(self.base)
 
-        modules = self.getModules()
-        self.fixComments()
-        self.draw(modules)
+        self.aoiItem = TreeItem(["AOI","Revision","Edited"], self.base)
+        self.udtItem = TreeItem(["UDT","Decription","Type"], self.base)
 
-    def getModules(self):
-        modules = []
+        self.base.appendChild(self.aoiItem)
+        self.base.appendChild(self.udtItem)
 
-        for module in self.root.iter("Module"):
+        self.aois = self.getAois()
 
-            inputs = []
-            outputs = []
+        self.udts = self.getUdts()
 
-            address = module.findall("./Ports/")[0].attrib["Address"]
-            hardware = module.attrib['ParentModule']
-            hcomment = module.attrib['Name']
+        self.draw()
 
-            if hcomment.find("Cube") != -1 or hcomment.find("K070") != -1:
-                hardware = hcomment
-                hcomment = ""
-            else:
-                hardware = hardware + ":" + address
+    def getAois(self):
+        print("gettin")
 
-            for search in ["InputTag", "InAliasTag", "OutputTag", "OutAliasTag"]:
-                for tags in module.iter(search):
-                    for comment in tags.iter("Comment"):
-                        comment.text = ET.CDATA(comment.text.replace("\n"," "))
-                        if search[0] == "I":
-                            inputs.append({ "hardware" : hardware + ":" + search[0] + comment.attrib['Operand'],
-                                            "hcomment" : comment.text})
-                        else:
-                            outputs.append({ "hardware" : hardware + ":" + search[0] + comment.attrib['Operand'],
-                                             "hcomment" : comment.text})
+    def getUdts(self):
+        print("gettin")
 
-            inputs = [self.getParameterConnection(i) for i in inputs]
-            inputs = [self.getParameterComment(i) for i in inputs]
-
-            outputs = [self.getParameterConnection(o) for o in outputs]
-            outputs = [self.getParameterComment(o) for o in outputs]
-
-            modules.append({'hardware'  : hardware,
-                            'hcomment'  : hcomment,
-                            'inputs'    : inputs,
-                            'outputs'   : outputs})
-
-        return modules
-
-
-    def getParameterComment(self, comment):
-        s = comment['parameter'].split('.')
-        if len(s) != 1:
-            programName = comment['parameter'].split('.')[0][1:]
-            tagName = comment['parameter'].split('.')[1]
-
-            for program in self.root.iter("Program"):
-                if program.attrib['Name'] == programName:
-                    for tag in program.iter("Tag"):
-                        if tag.attrib['Name'] == tagName:
-                            potential = tag.findall("Description")
-                            if len(potential):
-                                potential[0].text = ET.CDATA(potential[0].text.replace("\n", " "))
-                                comment['pcomment'] = potential[0].text
-                                return comment
-
-        comment['pcomment'] = ''
-        return comment
-
-    def getParameterConnection(self, comment):
-        for parameter in self.root.iter("ParameterConnection"):
-            e1 = parameter.attrib['EndPoint1']
-            e2 = parameter.attrib['EndPoint2']
-
-            if e1[0] == "\\":
-                hw = e2
-                tag= e1
-            else:
-                hw = e1
-                tag= e2
-
-            hw = hw.split(":")
-            hw[1] = hw[1].upper()
-            hw = ":".join(hw)
-
-            if comment['hardware'] == hw:
-                comment['parameter'] = tag
-                return comment
-
-        comment['parameter'] = ""
-        return comment
-
-    def highlight(self):
-        self.isHighlighted = not self.isHighlighted
-
-    def draw(self, modules):
-        for module in modules:
-            if len(module['inputs']) or len(module['outputs']):
-                item = TreeItem([module['hardware'], module['hcomment']], self.base, 1)
-                self.base.appendChild(item)
-
-                inputs = TreeItem(["Inputs"], item, 2)
-                item.appendChild(inputs)
-                self.treeHardware(inputs, module['inputs'])
-
-                outputs = TreeItem(["Outputs"], item, 2)
-                item.appendChild(outputs)
-                self.treeHardware(outputs, module['outputs'])
-
-    def treeHardware(self, parent, puts):
-        for put in puts:
-            if len(put['hardware'].split(".")) == 2:
-                childItem = TreeItem([put['hardware'], put['hcomment'], put['parameter'], put['pcomment']], parent, 3)
-                parent.appendChild(childItem)
-
-        for put in puts:
-            pModule = put['hardware'].split(".")
-            if len(pModule) == 3:
-                pModule = ".".join(pModule[0:2])
-
-                root = self.base
-                hwCount = root.childCount()
-                for i in range(hwCount):
-
-                    hardware = root.child(i)
-                    childCount = hardware.childCount()
-                    for j in range(childCount):
-
-                        p = hardware.child(j)
-                        moduleCount = p.childCount()
-                        for k in range(moduleCount):
-
-                            module = p.child(k)
-                            if module.data(0) ==  pModule:
-                                childItem = TreeItem([put['hardware'], put['hcomment'], put['parameter'], put['pcomment']], module, 4)
-                                module.appendChild(childItem)
-
+    def draw(self):
+        print("drawing")
